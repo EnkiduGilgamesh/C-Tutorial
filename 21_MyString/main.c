14b38c1510a2ae027b7bdcdf4fd24bb0b798370e
YounG <SB-YounG@outlook.com>
2022-12-02 16:50:01 +0800

    time test for mystring

--- 21_MyString/main.c
+++ 21_MyString/main.c
@@ -7,7 +7,7 @@
 * Author: Wenren Muyan                                                                             *
 * Comments:                                                                                        *
 * --------------------------------------------------------------------------------                 *
-* Last Modified: 1/12/2022 09:21:43                                                                *
+* Last Modified: 2/12/2022 04:48:0                                                                 *
 * Modified By: Wenren Muyan                                                                        *
 * --------------------------------------------------------------------------------                 *
 * Copyright (c) 2022 - future Wenren Muyan                                                         *
@@ -24,27 +24,70 @@
 #include <time.h>
 #include "mystring.h"
 
+#define MAX_SIZE 100000000
+
 int main(){
-    char s[] = "ASDHKSAFHGHKVJDBXVASDYUSGCXBVGEWRUYGFBVJHDVJXGAKSFHCKXZBVJCBXJVBJHSGDFHDJSKDFNKJBVJCXBJHGAABBABABAABABAAB";
+    char sb[] = "ASDHKSAFHGHKVJDBXVASDYUSGCXBVGEWRUYGFBV\
+        JHDVJXGAKSFHCKXZBVJCBXJVBJHSGDFHDJSKDFNKJBVJCXB\
+        ASBCKXZNKNVFKDVNVKDFVJDSVNASJFACNXZCNGJRLLADEVC\
+        ASDEIFITUOVNCNASDEFRHYJUJUKOIKGFVASDVCVXVKNNABA\
+        ASFHRHGJNVXNCVKNFJHGHASDCZXCBBJDJFSVNXVKJXJCCSD\
+        ASDSJKNCNVCNXVNCXNVNLFKELALDJSNCVJNEOWFJLSDFSNV\
+        DDDDDDDDDDDDDDDDDDDDDDASDSACZFEGBASVFXBDSGTGFDG\
+        ASCNLZNLRNVNNADSCNJVNVLSDKSALCMMCFVFJVNJLALAMVV\
+        ASIHCOXOVNFLENROFDSLKVNLKFLGLNBNHOASDDJCNZLCNRN\
+        ASODIORJFNVNCLASKDEJFIROGJJJASLKMKLXNCJNFJDVNNO\
+        SAIHCUHJCFORJRGKVDCCXLVJFJASDKCMXZCMFJEIJQPIPWK\
+        ACOIJCOVNFVJNFIFPASDKASMCFKVTNPFLKDJFVJNVNMVKJA\
+        UAFOIJDISVKNVLCMVLKALFJJFRIOJZVKNKFLDKIPAAKDSSV\
+        SADSACNDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD\
+        ASDADWQSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS\
+        ZXCDFSDUVJCXVDSSSSFEFASDDDDDDDDDDDDDDDDDDDDDDDD\
+        ASDEFSDJCZVZVNJFJASABABSADWDACZCBBBAAAASDSAAABS\
+        ASDSACZCFAAABABBABABASADCDSFEDSADSCABABASDSACZC\
+        ABABAASCJZCFJBBBASDASDABBBSDADSAAAAAASADEFSDCBX\
+        ZXCHFRASDAANANNABABASDADCACXZCDABABASCDFAAAAABA\
+        ACZCZCABABDEFFFASDACXZCABABADSDFEFSASCAFEFABABS\
+        ADCCFSDSAFDFJOLJJNNNADJAOABABACJJJDJJFESKKCNLLA\
+        UADAABSDEDSABABCJJCRFKCKJZSHHHHHHDEFFJJJJJJJJHA\
+        ASDHEFHABABBASJAJCJOCJOJEFEFJVOOJCAJDFHROFJJFJF\
+        AFHRHFNCHAIDJJCJACNCNANCNASDHNCZXCVEFEFFASDSACC\
+        JHGAABBABABAABABAAB";
     char sub[] = "ABABAABAB";
     char good[] = "abcdefeghde";
     char subgood[] = "cde";
+    char * s = (char *)malloc(sizeof(char) * MAX_SIZE);
+    for(int i = 0; i < MAX_SIZE - 9; i++){
+        s[i] = 'A' + (int)(rand() / 1261);
+    }
+    strcat(s, sub);
     // int len = strlen(good);
     // int * goodSufFix = (int *)malloc(sizeof(int) * len);
 
-    time_t t;
-    time(&t);
-    printf("Time: %s\n", ctime(&t));
+    // time_t t;
+    // time(&t);
+    // printf("Time: %s\n", ctime(&t));
+    clock_t begin, end;
+    begin = clock();
     int pos = findBF(s, sub);
+    end = clock();
     printf("%d\n", pos);
-    time(&t);
-    printf("Time: %s\n", ctime(&t));
+    double duration = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Time: %lfs\n", duration);
+    // time(&t);
+    // printf("Time: %s\n", ctime(&t));
+    begin = clock();
     pos = findKMP(s, sub);
+    end = clock();
     printf("%d\n", pos);
-    printf("Time: %s\n", ctime(&t));
+    duration = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Time: %lfs\n", duration);
+    begin = clock();
     pos = findBM(s, sub);
+    end = clock();
     printf("%d\n", pos);
-    printf("Time: %s\n", ctime(&t));
+    duration = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Time: %lfs\n", duration);
 
     // genBMGoodFix(good, len, goodSufFix);
 
